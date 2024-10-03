357bca58febd2b016038b19bcef43667ad24bc86
Jasper Phillip <145681364+RivenSaint0855@users.noreply.github.com>
2024-10-03 15:55:30 +0800

    Create Lamp.java

---
+++ Lamp.java
@@ -0,0 +1,12 @@
+class Lamp{
+	public String name;
+	boolean isOn;
+	void turnOn(){
+		isOn = true;
+		System.out.println("Light On? " + isOn);
+	}
+	void turnOff(){
+		isOn = false;
+		System.out.println("Light Off? " + isOn);
+	}
+}
